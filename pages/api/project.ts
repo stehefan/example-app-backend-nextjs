e1c6a083669e9cac168e079307d0ed08e329123f
Stefan Lier <stefan.lier@aerq.com>
2023-09-16 13:03:27 +0200

    :recycle: Externalize ErrorResponse Type

--- pages/api/project.ts
+++ pages/api/project.ts
@@ -1,26 +1,22 @@
 import type {NextApiRequest, NextApiResponse} from 'next'
 import {PrismaClient, Project} from "@prisma/client";
 
-type ResponseData = {
-    message: string
-}
-
 const prisma = new PrismaClient();
 
 export default async function handler(
     req: NextApiRequest,
-    res: NextApiResponse<ResponseData | Project | Project[]>
+    res: NextApiResponse<Project | Project[] | ErrorResponse>
 ) {
     if (req.method === 'POST') {
         const projectToCreate: Project = req.body;
 
         const project: Project = await prisma.project.create({
             data: projectToCreate
-        })
+        });
 
         res
             .status(200)
-            .json(project)
+            .json(project);
     } else if (req.method === 'GET') {
         const projects: Project[] = await prisma.project.findMany();
 
@@ -31,7 +27,7 @@ export default async function handler(
         res
             .status(405)
             .json({
-                message: 'Method not allowed'
+                error: 'Method not allowed'
             })
     }
 }
---
+++ types/global.d.ts
@@ -0,0 +1,8 @@
+type ValidationError = {
+    status: number,
+    message: string
+}
+
+type ErrorResponse = {
+    error: string
+}
