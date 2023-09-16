cfddc2394c1d343700970dcbb26fa3b91e0025ed
Stefan Lier <stefan.lier@aerq.com>
2023-09-16 13:03:52 +0200

    :sparkles: Add CRUD Management of Projects

--- pages/api/project/[projectId].ts
+++ pages/api/project/[projectId].ts
@@ -1,16 +1,91 @@
 import {NextApiRequest, NextApiResponse} from "next";
+import {PrismaClient, Project} from "@prisma/client";
 
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+const prismaClient = new PrismaClient();
+
+function validateProjectId(projectId: any): ValidationError | null {
+    if (!projectId) {
+        return {
+            status: 400,
+            message: 'No ProjectID given'
+        };
+    } else if (Array.isArray(projectId)) {
+        return {
+            status: 400,
+            message: 'More than one projectId given'
+        };
+    } else if (Number.isNaN(Number(projectId))) {
+        return {
+            status: 400,
+            message: `Not a valid projectId: \'${projectId}\'`
+        };
+    }
+    return null;
+}
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Project | ErrorResponse>) {
     const {projectId} = req.query;
 
-    if (!projectId) {
+    const validationResult = validateProjectId(projectId);
+    if (validationResult !== null) {
         res
-            .status(400)
-            .json({error: 'No ProjectID given'});
+            .status(validationResult.status)
+            .json({
+                error: validationResult.message
+            });
     } else {
-        res
-            .status(200)
-            .json({message: 'retrieved projectId ' + projectId});
+        const parsedProjectId: number = Number(projectId);
+        const project: Project | null = await prismaClient.project.findFirst({
+            where: {
+                id: parsedProjectId
+            }
+        })
+        if (project == null) {
+            res
+                .status(404)
+                .json({error: `Project with projectId \'${projectId}\' not found`});
+        } else {
+            if (req.method === 'GET') {
+                res
+                    .status(200)
+                    .json(project);
+            } else if (req.method === 'PUT') {
+                const projectUpdateContent: Project = req.body;
+                if (projectUpdateContent.id !== parsedProjectId) {
+                    res
+                        .status(412)
+                        .json({
+                            error: 'projectIds do not match'
+                        })
+                } else {
+                    const updatedProject = await prismaClient.project.update({
+                        where: {
+                            id: parsedProjectId
+                        },
+                        data: projectUpdateContent
+                    });
+
+                    res.status(200).json(updatedProject);
+                }
+            } else if (req.method === 'DELETE') {
+                const deletedProject = await prismaClient.project.delete({
+                    where: {
+                        id: parsedProjectId
+                    }
+                });
+                res
+                    .status(200)
+                    .json(deletedProject);
+            } else {
+                res
+                    .status(405)
+                    .json({
+                        error: 'Method not allowed'
+                    });
+            }
+
+        }
     }
 }
+
